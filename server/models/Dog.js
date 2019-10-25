ff6bc8bf1b13742152e0348e2a19ddbce6cb397d
Andy Ong <axo5586@rit.edu>
2019-10-25 03:32:23 -0400

    uploading hw

---
+++ server/models/Dog.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+let DogModel = {};
+
+const DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    breed: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+        trim: true,
+        min: 0,
+    },
+    createdDate: {
+        type: Number,
+        default: Date.now,
+    },
+    
+    // createdDate: {
+    //     type: Date,
+    //     default: Date.now,
+    // },
+});
+
+DogSchema.statics.sayName = (dog) => {
+    console.log(dog.name);
+}
+
+DogSchema.statics.addAge = (dog) => {
+    dog.age++;
+}
+
+DogSchema.statics.findByName = (name, callback) => {
+    const search = {
+        name,
+    };
+
+    return DogModel.findOne(search, callback);
+}
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
